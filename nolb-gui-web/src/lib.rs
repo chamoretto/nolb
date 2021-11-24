a2a13a7f98f10f59c9ab4a7f04f8d741a2a5c836
chamoretto <chamor.work@gmail.com>
2021-11-24 12:06:49 +0300

    Turn nolb-gui-web into lib (because of wasm)

--- nolb-gui-web/src/lib.rs
+++ nolb-gui-web/src/lib.rs
@@ -15,6 +15,11 @@
     clippy::same_functions_in_if_condition
 )]
 
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
 }
